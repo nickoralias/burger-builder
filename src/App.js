13b931e9125010f16aa4931f59030aab4a984cb0
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-08-23 09:53:16 -0700

    Add Layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
+
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
-  );
+  )
 }
 
 export default App;
