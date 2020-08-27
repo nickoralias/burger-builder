4f28af5f0a0a743c85eec1ce18896a1583e109fd
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-08-26 23:07:54 -0700

    Connect state to build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENT_PRICES = {
+  lettuce: 0.5,
+  cheese: 0.4,
+  patty: 1.3,
+  bacon: 0.7,
+};
+
 class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
@@ -13,15 +20,31 @@ class BurgerBuilder extends Component {
       lettuce: 0,
       bacon: 0,
       cheese: 0,
-      patty: 0
-    }
-  }
+      patty: 0,
+    },
+    totalPrice: 4,
+  };
 
-  render () {
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = (type) => {};
+
+  render() {
     return (
       <React.Fragment>
-        <Burger ingredients={this.state.ingredients}/>
-        <BuildControls />
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </React.Fragment>
     );
   }
